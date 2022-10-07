aa8a2fd1908fa4037baabdbb0fb3e353e8d25783
pkck28 <kcpkumar33gmail.com>
2022-10-07 17:01:34 -0400

    sasena test function - single and multi - added.

--- examples/datgen_rosenbrock.py
+++ examples/datgen_rosenbrock.py
@@ -1,18 +1,12 @@
 from datgen import Rosenbrock
 
 options = {
-    "numberOfSamples" : 5,
-    "samplingMethod" : "lhs",
-    "lowerBound" : [-2, -2],
-    "upperBound" : [2, 2],
-    "directory" : "rosenbrock",
     "parameters" : {
         "a" : 1,
-        "b" : 100,
-        "c" : 1
+        "b" : 100
     }
 }
 
-test = Rosenbrock(options=options)
+test = Rosenbrock(type="single")
 
-test.generateSamples()
+test.getObjectives([0, 1])
