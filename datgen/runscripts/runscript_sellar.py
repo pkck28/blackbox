a1e9d5e092b1987b4bfed964b272c728126bfa2a
Pavankumar Koratikere <kcpkumar33@gmail.com>
2022-11-08 14:53:10 -0500

    small changes in sellar runscript

--- datgen/runscripts/runscript_sellar.py
+++ datgen/runscripts/runscript_sellar.py
@@ -34,7 +34,6 @@ class SellarDis1(om.ExplicitComponent):
         x3 = inputs['x'][2]
         y2 = inputs['y2']
 
-        # outputs['y1'] = x1**2 + x2 + x3 - 0.2*y2
         outputs['y1'] = x1**2 + x2 + x3 - 0.2*y2
 
 # Class for Displine 2
@@ -71,9 +70,8 @@ class SellarDis2(om.ExplicitComponent):
         if y1.real < 0.0:
             y1 *= -1
 
-        outputs['y2'] = y1**0.5 + x1 + x2
-        # outputs['y2'] = y1**0.5 + x1 + x3
-        # outputs['y2'] = y1**2 + x1 + x2
+        # outputs['y2'] = y1**0.5 + x1 + x2
+        outputs['y2'] = y1**0.5 + x1 + x3
 
 # Class for MDA
 class SellarMDA(om.Group):
@@ -98,11 +96,11 @@ class SellarMDA(om.Group):
 
         # Nonlinear Block Gauss Seidel is a gradient free solver
         # iprint = 0 stops printing mda sovler details
-        cycle.nonlinear_solver = om.NonlinearBlockGS(iprint=0, maxiter=20, use_aitken=True)
+        cycle.nonlinear_solver = om.NonlinearBlockGS(iprint=0, maxiter=50, use_aitken=True)
 
         # Adding output components
-        # self.add_subsystem('obj_comp', om.ExecComp('obj = x[0]**2 + x[2] + y1 + exp(-y2)', x=np.zeros(3)), promotes=['x', 'y1', 'y2', 'obj'])
-        self.add_subsystem('obj_comp', om.ExecComp('obj = x[2]**2 + x[1] + y1 + exp(-y2)', x=np.zeros(3)), promotes=['x', 'y1', 'y2', 'obj'])
+        self.add_subsystem('obj_comp', om.ExecComp('obj = x[0]**2 + x[2] + y1 + exp(-y2)', x=np.zeros(3)), promotes=['x', 'y1', 'y2', 'obj'])
+        # self.add_subsystem('obj_comp', om.ExecComp('obj = x[2]**2 + x[1] + y1 + exp(-y2)', x=np.zeros(3)), promotes=['x', 'y1', 'y2', 'obj'])
         self.add_subsystem('con_comp1', om.ExecComp('con1 = 1 - y1/3.16'), promotes=['con1', 'y1'])
         self.add_subsystem('con_comp2', om.ExecComp('con2 = y2/24.0 - 1'), promotes=['con2', 'y2'])
 
