3ac77e2d428279426311453168aea8ce17328487
Pavankumar Koratikere <kcpkumar33@gmail.com>
2022-11-09 14:18:36 -0500

    change in version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datgen',
-    version='0.0.2',
+    version='0.0.5',
     packages=find_packages(where=".", include=['datgen']),
     install_requires=[],
 )
