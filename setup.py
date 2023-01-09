b7402cfeed24b1d26db086e2cf6f132a2637cd84
Pavankumar Koratikere <kcpkumar33@gmail.com>
2023-01-09 14:30:20 -0500

    added dependencies in setup.py file

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='blackbox',
     version='0.0.6',
     packages=find_packages(include=['blackbox*']),
-    install_requires=["numpy", "scipy"],
+    install_requires=["numpy>=1.21.6", "scipy>=1.7.3", "mpi4py=3.1.3", "pygeo>=1.12.2", "prefoil>=2.0.0", "pyDOE2>=1.3.0"],
 )
