fixed area computation method

blackbox/physics_models/airfoil/airfoil_cst_multipoint.py
@@ -74,12 +74,12 @@ class AirfoilCSTMultipoint(AirfoilBaseClass):
             os.system("mkdir {}".format(directory))
 
         # Read the coordinate file
-        self.coords = readCoordFile(self.options["airfoilFile"])
+        self.origCoords = readCoordFile(self.options["airfoilFile"])
 
         # Some validation for coordinate file
-        if self.coords[0,0] != self.coords[-1,0]:
+        if self.origCoords[0,0] != self.origCoords[-1,0]:
             self._error("The X coordinate of airfoil doesn't start and end at same point.")
-        elif self.coords[0,1] != self.coords[-1,1]:
+        elif self.origCoords[0,1] != self.origCoords[-1,1]:
             self._error("The Y coordinate of airfoil doesn't start and end at same point.")
 
         # Initializing the parametrization object
@@ -87,13 +87,13 @@ class AirfoilCSTMultipoint(AirfoilBaseClass):
         self.parametrization = "CST"
 
         # Adding pointset to the parametrization
-        self.coords = np.hstack(( self.coords, np.zeros((self.coords.shape[0], 1)) ))
-        self.DVGeo.addPointSet(self.coords, "airfoil")
+        self.origCoords = np.hstack(( self.origCoords, np.zeros((self.origCoords.shape[0], 1)) ))
+        self.DVGeo.addPointSet(self.origCoords, "airfoil")
 
         # Checking the number of points at trailing edge for blunt TE
         # Only two are allowed for CST. Otherwise, meshing will have problem.
         if not self.DVGeo.sharp:
-            if len(np.where(self.coords[1:-1,0] == self.coords[0,0])[0]) > 1:
+            if len(np.where(self.origCoords[1:-1,0] == self.origCoords[0,0])[0]) > 1:
                 self._error("There are more than two points in the trailing edge.")
 
         # Some initializations which will be used later